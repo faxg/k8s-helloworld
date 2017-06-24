dc4920c77285afa057dff5e0fc235e1aa3e1729f
Florian Georg <florian.alexander.georg@gmail.com>
2017-06-24 20:02:01 +0200

    changed msg

--- tag.sh
+++ tag.sh
@@ -1,5 +1,5 @@
 
 IMG=faxg/hello-world-web:$1
-echo "Tagging and push: $IMG"
+echo "Tagging and push: faxg/hello-world-web:latest --> $IMG"
 docker tag faxg/hello-world-web:latest $IMG
 docker push $IMG
