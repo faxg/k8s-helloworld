239da9750863550b0632e405bda75cc189798f03
Florian Georg <florian.alexander.georg@gmail.com>
2017-06-25 22:46:24 +0200

    --record and externalIP

--- k8s_deploy.sh
+++ k8s_deploy.sh
@@ -3,13 +3,14 @@ export NS=hello-world
 echo "Using K8S namespace '$NS' ..."
 
 # make sure namespace exists
-kubectl apply -f ./manifests/namespace.yaml
+kubectl apply -f ./manifests/namespace.yaml --record
 
 # create configMaps, db backend (redis) and web-ui deployment
-kubectl apply -f ./manifests/config.yaml --namespace="$NS"
-kubectl apply -f ./manifests/deploy-webui.yaml --namespace="$NS"
-kubectl apply -f ./manifests/deploy-db.yaml --namespace="$NS"
+kubectl apply -f ./manifests/config.yaml --namespace="$NS" --record
+kubectl apply -f ./manifests/deploy-webui.yaml --namespace="$NS" --record
+kubectl apply -f ./manifests/deploy-db.yaml --namespace="$NS" --record
 
 
 
 # ssh into container: kubectl --namespace=hello-world exec -it web-ui-deployment-1287265622-qvtrl -- /bin/sh
+# kubectl rollout status deploy/web-ui-deployment --namespace="hello-world"
